7b7d5464945c49198c691ef1490714abe7e3c260
Neil Bouiche <neil.bouiche@icloud.com>
2023-03-22 08:51:22 +0100

    Call Api Qestions

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Home from './pages/Home'
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./pages/Home";
+import Survey from "./pages/Survey";
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import Error from "./components/Error";
+import Freelances from "./pages/Freelances";
+import Results from "./pages/Results";
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+    body {
+      margin: 0;
+    }
+`;
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/survey/:questionNumber" element={<Survey />}></Route>
+        <Route path="*" element={<Error />}></Route>
+        <Route path="/freelances" element={<Freelances />}></Route>
+        <Route path="/results" element={<Results />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
